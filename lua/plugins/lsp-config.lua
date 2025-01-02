424d13a4035e6aa7ec155f27d188fb17c15f73d1
stanleyavril123 <stanley.avril@outlook.com>
2025-01-02 07:48:18 -0500

    removed eslint_d diagnostics

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,27 +1,28 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+			ensure_installed = { "lua_ls", "ts_ls", "pyright" },
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
 
 			lspconfig.lua_ls.setup({
-        capabilities = capabilities
+				capabilities = capabilities,
 			})
 			lspconfig.ts_ls.setup({ capabilities = capabilities })
 			lspconfig.pyright.setup({ capabilities = capabilities })
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,23 +1,22 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        require("none-ls.diagnostics.eslint_d"),
-        -- formatting
-        -- lua
-        null_ls.builtins.formatting.stylua,
-        -- python
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.isort,
-        -- javaScript
-        null_ls.builtins.formatting.prettierd,
-      },
-    })
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				-- formatting
+				-- lua
+				null_ls.builtins.formatting.stylua,
+				-- python
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.isort,
+				-- javaScript
+				null_ls.builtins.formatting.prettierd,
+			},
+		})
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
