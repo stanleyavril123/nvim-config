4f0577da4cc26a97806ef2b8a2142a5d9ab2212b
Stanley Avril <stanley.avril@outlook.com>
2025-01-04 14:33:25 -0500

    telescope ignore files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,19 @@
-return {    -- Telescope plugin
-{    
+return {
+  {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
+      require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = { "venv/", "%.pyc", "__pycache__/" },
+        },
+      })
+
       -- Key mapping for Telescope
       local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
     end,
+  },
 }
-}
+
