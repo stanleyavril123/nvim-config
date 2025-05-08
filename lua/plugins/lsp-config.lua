0dbd3197879363f6685e1d8404b13c77bf63f431
Stanley Avril <stanley.avril@outlook.com>
2025-05-08 17:48:06 -0400

    go to reference shortcut added

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -31,7 +31,8 @@ return {
 			lspconfig.html.setup({ capabilities = capabilities })
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
 	},
