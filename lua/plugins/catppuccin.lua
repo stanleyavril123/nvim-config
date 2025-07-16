77ea1fe5f7c5e27f7c4191c251f186f939a17905
Stanley <stanley.avril@outlook.com>
2025-07-16 10:26:28 -0400

    added dark background

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,14 +1,27 @@
 return {
-  -- Plugin specifications
-  {
-    -- Catppuccin theme
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup()
-      vim.cmd.colorscheme("catppuccin")
-    end,
-  }
-}
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			vim.opt.termguicolors = true
+			vim.opt.background = "dark"
 
+			require("catppuccin").setup({
+				flavour = "mocha",
+				transparent_background = false,
+				custom_highlights = function(cp)
+					return {
+						Normal = { bg = "#000000" },
+						NormalNC = { bg = "#000000" },
+						SignColumn = { bg = "#000000" },
+						Folded = { bg = "#000000" },
+						WinSeparator = { fg = cp.surface1, bg = "#000000" },
+					}
+				end,
+			})
+
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+}
